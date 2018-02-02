27ee895f65a1b1c9732ac24b3bbb10e8aaae7c64
vkdlxh <tkdwnszoq@naver.com>
2018-02-02 16:35:17 +0900

    CurrencyLabel イメージ実装方法変更

--- DesignPatterns/DesignPatterns/Utilities/CurrencyLabel.swift
+++ DesignPatterns/DesignPatterns/Utilities/CurrencyLabel.swift
@@ -10,26 +10,23 @@ import UIKit
 
 class CurrencyLabel: UILabel {
 
-    let padding = UIEdgeInsets(top: 2, left: 8, bottom: 2, right: 8)
+    var imageView = UIImageView()
+    
     override func drawText(in rect: CGRect) {
+        let padding = UIEdgeInsets(top: 0, left: imageView.frame.width + 10, bottom: 0, right: 0)
         super.drawText(in: UIEdgeInsetsInsetRect(rect, padding))
     }
-
     
     func countyFlag(county: Country) {
-        guard let currency = CurrencyFactory.currency(for: county) else {
-            return
-        }
-        let imageAttachment =  NSTextAttachment()
-        imageAttachment.image = UIImage(named: county.rawValue)
-        let attachmentString = NSAttributedString(attachment: imageAttachment)
-        let imageOffsetY:CGFloat = -25.0;
-        imageAttachment.bounds = CGRect(x: 0, y: imageOffsetY, width: 60, height: 60)
-        let completeText = NSMutableAttributedString(string: "")
-        completeText.append(attachmentString)
-        let textAfterIcon = NSMutableAttributedString(string: currency.code())
-        completeText.append(textAfterIcon)
-        self.attributedText = completeText
+        let imageName = county.rawValue
+        let image = UIImage(named: imageName)
+        imageView = UIImageView(image: image!)
+        let middleOflabelHeight = self.frame.origin.y
+        print(middleOflabelHeight)
+        imageView.frame = CGRect(x: 0, y: -middleOflabelHeight, width: 60, height: 50)
+        self.addSubview(imageView)
+        
+        self.text = county.rawValue
     }
 
 }
